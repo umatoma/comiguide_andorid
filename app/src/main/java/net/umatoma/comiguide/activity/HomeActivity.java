288cba493c5e6333750f8fcd53e50e326a234d62
umatoma <umatomakun@gmail.com>
2015-02-11 19:25:40 +0900

    ユーザー情報を表示する

--- app/src/main/java/net/umatoma/comiguide/activity/HomeActivity.java
+++ app/src/main/java/net/umatoma/comiguide/activity/HomeActivity.java
@@ -4,15 +4,31 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import net.umatoma.comiguide.R;
+import net.umatoma.comiguide.model.User;
 
 public class HomeActivity extends ActionBarActivity {
+
+    private User mUser;
+    private ImageView mUserIcon;
+    private TextView mUserName;
+    private ListView mNotificationList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        mUser = new User(this);
+        mUserIcon = (ImageView) findViewById(R.id.userIcon);
+        mUserName = (TextView) findViewById(R.id.userName);
+        mNotificationList = (ListView) findViewById(R.id.notificationList);
+
+        mUserName.setText(mUser.getUserName());
     }
 
 
