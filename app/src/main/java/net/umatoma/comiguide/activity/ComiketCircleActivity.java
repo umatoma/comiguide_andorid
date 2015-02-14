669e97afabc09be8d1d8b09321a499aabffb3901
umatoma <umatomakun@gmail.com>
2015-02-15 00:24:11 +0900

    click functions button

--- app/src/main/java/net/umatoma/comiguide/activity/ComiketCircleActivity.java
+++ app/src/main/java/net/umatoma/comiguide/activity/ComiketCircleActivity.java
@@ -4,15 +4,29 @@ import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import com.getbase.floatingactionbutton.FloatingActionButton;
 
 import net.umatoma.comiguide.R;
 
 public class ComiketCircleActivity extends ActionBarActivity {
 
+    private FloatingActionButton mFunctionsButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_comiket_circle);
+
+        mFunctionsButton = (FloatingActionButton) findViewById(R.id.functions_button);
+        mFunctionsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(ComiketCircleActivity.this, "click", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
 
