1ffd1319afb1683cc0d8fe3c4b5720279cf94a8a
umatoma <umatomakun@gmail.com>
2015-02-14 17:21:55 +0900

    shoe setting menu

--- app/src/main/java/net/umatoma/comiguide/activity/SettingsActivity.java
+++ app/src/main/java/net/umatoma/comiguide/activity/SettingsActivity.java
@@ -2,6 +2,7 @@ package net.umatoma.comiguide.activity;
 
 import android.app.Activity;
 import android.preference.PreferenceActivity;
+import android.preference.PreferenceFragment;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -14,6 +15,18 @@ public class SettingsActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_settings);
+
+        getFragmentManager().beginTransaction()
+                .replace(android.R.id.content, new AccountSettingFragment())
+                .commit();
+    }
+
+    public static class AccountSettingFragment extends PreferenceFragment {
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+
+            addPreferencesFromResource(R.xml.account_preferences);
+        }
     }
 }
