1c7c764906b2cd08e18fd871c3d55c493f4688e5
umatoma <umatomakun@gmail.com>
2015-02-28 22:01:37 +0900

    set form view

--- app/src/main/java/net/umatoma/comiguide/fragment/ComiketCircleFormFragment.java
+++ app/src/main/java/net/umatoma/comiguide/fragment/ComiketCircleFormFragment.java
@@ -1,12 +1,18 @@
 package net.umatoma.comiguide.fragment;
 
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import net.umatoma.comiguide.R;
 import net.umatoma.comiguide.model.ComiketCircle;
@@ -15,6 +21,17 @@ public class ComiketCircleFormFragment extends Fragment {
 
     public static final String TAG = "ComiketCircleFormFragment";
     private ComiketCircle mComiketCircle;
+    private ArrayAdapter<String> mComiketBlockAdapter;
+    private ArrayAdapter<String> mComiketLayoutAdapter;
+    private ArrayAdapter<String> mSpaceNoSubAdapter;
+    private Spinner mFormComiketBlock;
+    private Spinner mFormComiketLayout;
+    private Spinner mFormSpaceNoSub;
+    private EditText mFormCircleName;
+    private EditText mFormCircleUrl;
+    private EditText mFormComment;
+    private EditText mFormCost;
+    private Button mButtonSubmit;
 
     public ComiketCircleFormFragment() {
         // Required empty public constructor
@@ -25,11 +42,53 @@ public class ComiketCircleFormFragment extends Fragment {
     }
 
     @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+
+        int resId = android.R.layout.simple_spinner_dropdown_item;
+        mComiketBlockAdapter = new ArrayAdapter<>(getActivity(), resId);
+        mComiketLayoutAdapter = new ArrayAdapter<>(getActivity(), resId);
+        mSpaceNoSubAdapter = new ArrayAdapter<>(getActivity(), resId);
+    }
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_comiket_circle_form, container, false);
         view.setOnTouchListener(new OnCnacelListener()); // Not through touch event.
+
+        mFormComiketBlock = (Spinner) view.findViewById(R.id.form_comiket_block);
+        mFormComiketLayout = (Spinner) view.findViewById(R.id.form_comiket_layout);
+        mFormSpaceNoSub = (Spinner) view.findViewById(R.id.form_comiket_circle_space_no_sub);
+        mFormCircleName = (EditText) view.findViewById(R.id.form_comiket_circle_name);
+        mFormCircleUrl = (EditText) view.findViewById(R.id.form_comiket_circle_url);
+        mFormComment = (EditText) view.findViewById(R.id.form_comiket_circle_comment);
+        mFormCost = (EditText) view.findViewById(R.id.form_comiket_circle_cost);
+        mButtonSubmit = (Button) view.findViewById(R.id.form_submit);
+
+        if (mComiketCircle != null) {
+            mFormComiketBlock.setAdapter(mComiketBlockAdapter);
+            mFormComiketLayout.setAdapter(mComiketLayoutAdapter);
+            mFormSpaceNoSub.setAdapter(mSpaceNoSubAdapter);
+
+            mFormCircleName.setText(mComiketCircle.getCircleName());
+            mFormCircleUrl.setText(mComiketCircle.getCircleUrl());
+            mFormComment.setText(mComiketCircle.getComment());
+            mFormCost.setText(mComiketCircle.getCost());
+
+            mButtonSubmit.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    attemptSubmit();
+                }
+            });
+        }
+
         return view;
+    }
+
+    private void attemptSubmit() {
+        Toast.makeText(getActivity(), "attemptSubmit", Toast.LENGTH_SHORT).show();
     }
 
     private class OnCnacelListener implements View.OnTouchListener {
@@ -38,6 +97,5 @@ public class ComiketCircleFormFragment extends Fragment {
             return true;
         }
     }
-
 
 }
