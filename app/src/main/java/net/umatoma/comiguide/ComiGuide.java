86e8dd99bea3b88d9a16e42e2ed124506aad88f9
umatoma <umatomakun@gmail.com>
2015-03-08 15:00:05 +0900

    set application class

---
+++ app/src/main/java/net/umatoma/comiguide/ComiGuide.java
@@ -0,0 +1,11 @@
+package net.umatoma.comiguide;
+
+import android.app.Application;
+
+public class ComiGuide extends Application {
+
+    @Override
+    public void onCreate () {
+        super.onCreate();
+    }
+}
