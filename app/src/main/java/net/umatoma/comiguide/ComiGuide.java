9f8d15fc679f765356817f72fe95c60d8e9fa48d
umatoma <umatomakun@gmail.com>
2015-03-08 16:26:47 +0900

    comiket_idを設定できる

--- app/src/main/java/net/umatoma/comiguide/ComiGuide.java
+++ app/src/main/java/net/umatoma/comiguide/ComiGuide.java
@@ -1,11 +1,22 @@
 package net.umatoma.comiguide;
 
 import android.app.Application;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 
 public class ComiGuide extends Application {
+
+    public static int COMIKET_ID = 87;
 
     @Override
     public void onCreate () {
         super.onCreate();
+
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        if (prefs.contains(getString(R.string.prefs_key_comiket_id))) {
+            COMIKET_ID = prefs.getInt(getString(R.string.prefs_key_comiket_id), COMIKET_ID);
+        } else {
+            prefs.edit().putInt(getString(R.string.prefs_key_comiket_id), COMIKET_ID).apply();
+        }
     }
 }
