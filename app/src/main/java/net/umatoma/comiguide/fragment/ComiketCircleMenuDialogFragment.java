a36928ea8226085d4f6f34be0cb01a42c7918fed
umatoma <umatomakun@gmail.com>
2015-03-25 22:53:36 +0900

    create MenuDialogFragment

--- app/src/main/java/net/umatoma/comiguide/fragment/ComiketCircleMenuDialogFragment.java
+++ app/src/main/java/net/umatoma/comiguide/fragment/ComiketCircleMenuDialogFragment.java
@@ -1,31 +1,22 @@
 package net.umatoma.comiguide.fragment;
 
 
-import android.app.AlertDialog;
 import android.app.Dialog;
 import android.os.Bundle;
-import android.support.v4.app.DialogFragment;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ListView;
 
 import net.umatoma.comiguide.R;
 import net.umatoma.comiguide.adapter.MenuListAdapter;
 import net.umatoma.comiguide.model.ComiketCircle;
 
-public class ComiketCircleMenuDialogFragment extends DialogFragment implements AdapterView.OnItemClickListener {
+import java.util.ArrayList;
+
+public class ComiketCircleMenuDialogFragment extends MenuDialogFragment {
 
     public static final String TAG = "ComiketCircleMenuDialogFragment";
 
     public static final int MENU_MAP = 1;
     public static final int MENU_EDIT = 2;
     public static final int MENU_DELETE = 3;
-
-    private ComiketCircle mCircle;
-    private ListView mMenuList;
-    private MenuListAdapter mAdapter;
-    private OnMenuDialogSelectListener mOnMenuDialogSelectListener;
 
     public static ComiketCircleMenuDialogFragment newInstance(ComiketCircle circle) {
         return new ComiketCircleMenuDialogFragment(circle);
@@ -36,58 +27,20 @@ public class ComiketCircleMenuDialogFragment extends DialogFragment implements A
     }
 
     private ComiketCircleMenuDialogFragment(ComiketCircle circle) {
-        mCircle = circle;
+        setTitle(circle.getCircleName());
     }
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
-        LayoutInflater inflater = getActivity().getLayoutInflater();
-        View view = inflater.inflate(R.layout.fragment_comiket_circle_menu_dialog, null, false);
-
-        mAdapter = new MenuListAdapter(getActivity());
-        mAdapter.add(new MenuListAdapter.MenuOption(
+        ArrayList<MenuListAdapter.MenuOption> list = new ArrayList<>();
+        list.add(new MenuListAdapter.MenuOption(
                 MENU_MAP, R.drawable.ic_map_marker, getString(R.string.dialog_comiket_circle_menu_show)));
-        mAdapter.add(new MenuListAdapter.MenuOption(
+        list.add(new MenuListAdapter.MenuOption(
                 MENU_EDIT, R.drawable.ic_edit, getString(R.string.dialog_comiket_circle_menu_edit)));
-        mAdapter.add(new MenuListAdapter.MenuOption(
+        list.add(new MenuListAdapter.MenuOption(
                 MENU_DELETE, R.drawable.ic_delete, getString(R.string.dialog_comiket_circle_menu_delete)));
+        setMenuOptionList(list);
 
-        mMenuList = (ListView) view.findViewById(android.R.id.list);
-        mMenuList.setAdapter(mAdapter);
-        mMenuList.setOnItemClickListener(this);
-
-        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-        if (mCircle != null) {
-            builder.setTitle(mCircle.getCircleName());
-            builder.setNegativeButton(R.string.dialog_cancel, null);
-            builder.setView(view);
-        }
-        return builder.create();
-    }
-
-    @Override
-    public void onResume () {
-        super.onResume();
-        if (mAdapter.getCount() == 0) {
-            dismiss();
-        }
-    }
-
-    @Override
-    public void onDetach () {
-        mOnMenuDialogSelectListener = null;
-        super.onDetach();
-    }
-
-    @Override
-    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-        if (mOnMenuDialogSelectListener != null) {
-            mOnMenuDialogSelectListener.onMenuSelect(mAdapter.getItem(position).menuId);
-        }
-        dismiss();
-    }
-
-    public void setOnMenuDialogSelectListener(OnMenuDialogSelectListener listener) {
-        mOnMenuDialogSelectListener = listener;
+        return super.onCreateDialog(savedInstanceState);
     }
 }
